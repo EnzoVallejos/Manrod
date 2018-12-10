8eb818701f1648db75ed765872ce65e975c7c00e
Enzo Vallejos <shotkill791@gmail.com>
2018-12-10 11:19:33 -0300

    Manrod v1.01

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'Manrod',
+  packages = ['Manrod'],
+  version = '1.01',
+  description = 'Manrod es una librería de código abierto que le permite generar palabras, frases, ordenar listas, entre otras funciones, de forma aleatoria.',
+  author = 'Enzo Vallejos',
+  author_email = 'shotkill791@gmail.com',
+  url = 'https://github.com/EnzoVallejos/Manrod',
+  download_url = 'https://github.com/EnzoVallejos/Manrod/tarball/1.01',
+  keywords = ['python', 'random', 'string', 'letters'],
+  classifiers = [],
+)
