68c6cf3a813018cb56f15c0e16e7e4ed3c3aa789
Enzo Vallejos <shotkill791@gmail.com>
2018-12-10 13:13:44 -0300

    Manrod v1.01-stable

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 from distutils.core import setup
+
+with open('README.md') as file:
+  long_description = file.read()
+
 setup(
   name = 'Manrod',
   packages = ['Manrod'],
-  version = '1.01',
+  version = '1.01-stable',
+  long_description=long_description,
   description = 'Manrod es una librería de código abierto que le permite generar palabras, frases, ordenar listas, entre otras funciones, de forma aleatoria.',
   author = 'Enzo Vallejos',
   author_email = 'shotkill791@gmail.com',
